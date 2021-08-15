687f9464a0ce9a74ae7bd6d9a5185fc04338e577
Ricardo Ruiz <ricardo@kenetic-labs.com>
2021-08-15 16:29:56 -0700

    First Commit

---
+++ marvel-comic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  marvel-comic
+//
+//  Created by Ricardo Ruiz on 8/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
